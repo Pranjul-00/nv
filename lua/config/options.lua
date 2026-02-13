e629614b7d91de3f8441a6355e2b91ec817940d2
Pranjul <pranjul.here@gmail.com>
2026-02-13 23:27:54 +0530

    .

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,7 +1,6 @@
-vim.opt.number = true           -- Show line numbers
-vim.opt.relativenumber = true   -- Relative numbers (makes jumping lines faster)
-vim.opt.cursorline = true       -- Highlight the line you're on
-vim.opt.tabstop = 4             -- Standard indent for C++ and Java
+vim.opt.number = true           -- Absolute line numbers
+vim.opt.relativenumber = true   -- Relative numbers for fast jumping
+vim.opt.cursorline = true       -- Highlight current line
+vim.opt.tabstop = 4             -- Standard indent for CIC assignments
 vim.opt.shiftwidth = 4
-vim.opt.expandtab = true        -- Use spaces instead of tabs
-vim.opt.scrolloff = 10          -- Keep code centered (10 lines visible above/below)
+vim.opt.expandtab = true
