7c6e1172307c0b73dc9ba7d9e0c7e87134daf841
Pranjul <pranjul.here@gmail.com>
2026-02-13 20:15:36 +0530

    .

---
+++ lua/config/lazy.lua
@@ -0,0 +1,12 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git", "clone", "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git", "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Tell lazy to look in the 'lua/plugins' folder for new stuff
+require("lazy").setup("plugins")
