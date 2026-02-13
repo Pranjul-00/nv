9d2b4a7eafe8f188e1f58a248e35acc5d3e294eb
Pranjul <pranjul.here@gmail.com>
2026-02-13 23:26:00 +0530

    Silenced LSP deprecation warnings in lsp-config.lua

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -17,6 +17,9 @@ return {
   {
     "neovim/nvim-lspconfig",
     config = function()
+      -- ADD THIS LINE HERE
+      vim.lsp.set_log_level("off") 
+
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({})
       lspconfig.clangd.setup({})  -- For your C++ DSA work
