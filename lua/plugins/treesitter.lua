10f0b3f4cf0cdd0d83f87b70a9bd45d955513ccf
Pranjul <pranjul.here@gmail.com>
2026-02-13 22:44:41 +0530

    fixed treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,7 +3,9 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
-      require("nvim-treesitter.configs").setup({
+      local status, ts = pcall(require, "nvim-treesitter.configs")
+      if not status then return end
+      ts.setup({
         ensure_installed = { "lua", "cpp", "python", "javascript" },
         highlight = { enable = true },
       })
