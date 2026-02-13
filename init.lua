7a72c0ee65a8b569496496501131e2b7cd30aec1
Pranjul <pranjul.here@gmail.com>
2026-02-13 20:10:47 +0530

    updatinf init.lua

--- init.lua
+++ init.lua
@@ -1,13 +1,1 @@
 require("config.lazy")
--- Basic Settings
-vim.opt.number = true          -- Show line numbers
-vim.opt.relativenumber = true  -- Relative numbers help with jumping lines
-vim.opt.mouse = 'a'            -- Enable mouse support (helpful for VS Code converts)
-vim.opt.ignorecase = true      -- Smart searching
-vim.opt.smartcase = true
-vim.opt.tabstop = 4            -- Tab size
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true       -- Use spaces instead of tabs
-
--- Set leader key (your 'shortcut' prefix, usually Space)
-vim.g.mapleader = " "
