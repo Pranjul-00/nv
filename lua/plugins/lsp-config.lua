ea6b1f9d339ed25a4aa70a28a8b1168a6a612d7a
Pranjul <pranjul.here@gmail.com>
2026-02-13 22:37:16 +0530

    setting lsp up

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,32 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        -- Automatically install these for your course work
+        ensure_installed = { "lua_ls", "clangd", "pyright" },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({})
+      lspconfig.clangd.setup({})  -- For your C++ DSA work
+      lspconfig.pyright.setup({}) -- For your Python/ML projects
+
+      -- Keybindings: Press 'K' to see documentation (hover)
+      -- Press 'gd' to Go to Definition
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+      vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
+    end,
+  }
+}
